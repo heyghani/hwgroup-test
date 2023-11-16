fed8fc8013c51f0d31643052bbab155c928696c3
Ghani <heyghani@gmail.com>
2023-11-16 11:34:43 +0700

    first commit

---
+++ hwgroup-test.js
@@ -0,0 +1,16 @@
+function printNumbers(x) {
+  for (let i = 1; i <= x; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("TigaLima");
+    } else if (i % 3 === 0) {
+      console.log("Tiga");
+    } else if (i % 5 === 0) {
+      console.log("Lima");
+    } else {
+      console.log(i);
+    }
+  }
+}
+
+let number = 15;
+printNumbers(number);
